7ff5b36af46670a0783aee38c13727d819914b88
xuezhao.wang <770240926@qq.com>
2022-05-23 23:17:32 +0800

    增加repository类

--- initial/src/main/java/com/example/restservice/company/CompanyRepository.java
+++ initial/src/main/java/com/example/restservice/company/CompanyRepository.java
@@ -1,4 +1,38 @@
 package com.example.restservice.company;
 
+import com.example.restservice.employee.Employee;
+import com.example.restservice.employee.EmployeeService;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Repository
 public class CompanyRepository {
+
+    public static List<Company> companyList = new ArrayList<>();
+
+    public List<Company> getCompanyList() {
+        return companyList;
+    }
+
+    public Company getCompanyById(Long id) {
+        return companyList.stream().filter(item -> item.getCompanyId() == id).findFirst().map(item -> item).orElse(null);
+    }
+
+    public void addCompany(Company company) {
+        companyList.add(company);
+    }
+
+    public void updateCompany(Long id, Company company) {
+        companyList.forEach(item -> {
+            if (item.getCompanyId() == id) {
+                item.setCompanyId(company.getCompanyId());
+                item.setCompanyName(company.getCompanyName());
+                item.setEmployeesNumber(company.getEmployeesNumber());
+                item.setEmployees(company.getEmployees());
+            }
+        });
+    }
 }
---
+++ initial/src/main/java/com/example/restservice/employee/EmployeeRepository.java
@@ -0,0 +1,42 @@
+package com.example.restservice.employee;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Repository
+public class EmployeeRepository {
+
+
+    public static List<Employee> employeeList = new ArrayList<>();
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void updateEmployee(int id, Employee employee) {
+        employeeList.forEach(item -> {
+            if (item.getId() == id) {
+                item.setId(employee.getId());
+                item.setName(employee.getName());
+                item.setAge(employee.getAge());
+                item.setGender(employee.getGender());
+                item.setSalary(employee.getSalary());
+            }
+        });
+    }
+    public Employee getEmployeeById(int id) {
+        AtomicReference<Employee> res = new AtomicReference<>();
+        employeeList.forEach(item->{
+            if (item.getId() == id) {
+                res.set(item);
+            }
+        });
+        return res.get();
+    }
+}
